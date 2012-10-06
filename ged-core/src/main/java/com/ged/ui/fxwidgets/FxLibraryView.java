3cfd838706e896e4fde86956b86ec2e31439c659
xaviermichel <xavier.michel.mx440@gmail.com>
2012-10-06 17:18:58 +0200

    Real library model for treeview

--- ged-core/src/main/java/com/ged/ui/fxwidgets/FxLibraryView.java
+++ ged-core/src/main/java/com/ged/ui/fxwidgets/FxLibraryView.java
@@ -1,5 +1,16 @@
 package com.ged.ui.fxwidgets;
 
+import java.io.File;
+import java.lang.ref.WeakReference;
+
+import javax.swing.DropMode;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import com.ged.Profile;
+import com.ged.ui.controllers.LibraryViewController;
+import com.tools.RelativeFileSystemModel;
+
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -17,6 +28,13 @@ import javafx.stage.Stage;
 
 public class FxLibraryView extends TreeView<String> {
 
+	
+	/**
+	 * TODO : fix this value usage
+	 */
+	private boolean showDirectoryOnly = false;
+	
+	
 	/*
  	private final Node rootIcon = new ImageView(
         new Image(getClass().getResourceAsStream("folder_16.png"))
@@ -24,15 +42,76 @@ public class FxLibraryView extends TreeView<String> {
     */
  	
     public FxLibraryView() {
-
-        TreeItem<String> rootItem = new TreeItem<String> ("Inbox"/*, rootIcon*/);
-        rootItem.setExpanded(true);
-        for (int i = 1; i < 6; i++) {
-            TreeItem<String> item = new TreeItem<String> ("Message" + i);            
-            rootItem.getChildren().add(item);
-        }        
-        
-        this.setRoot(rootItem);
+    	buildTree();
     }
 
+    
+	/**
+	 * Get the path to display in a tree node
+	 */
+	private static String convertToNodeName(String path) {
+		String[] files = path.split(File.separator.equals("\\") ? "\\\\" : File.separator);	// stupid windows fix...
+		return files[files.length - 1];
+	}
+    
+    
+    /**
+	 * Build the library tree, according to the registered library root
+	 */
+	private void buildTree() {
+
+		TreeItem<String> rootItem = new TreeItem<String>(convertToNodeName(Profile.getInstance().getLibraryRoot()));
+		
+		listFile(
+				new File(Profile.getInstance().getLibraryRoot()), 
+				rootItem
+		);
+		
+		rootItem.setExpanded(true);
+		this.setRoot(rootItem);
+		
+		/*
+		tree.setDragEnabled(true);
+		tree.setDropMode(DropMode.ON); 
+		
+		controller = new WeakReference<LibraryViewController>(new LibraryViewController(this));
+		tree.addMouseListener(controller.get());
+		tree.addTreeSelectionListener(controller.get());
+		tree.setTransferHandler(controller.get()); 		// for drag & drop
+		model.addTreeModelListener(controller.get());
+		*/
+	}
+	
+	
+	/**
+	 * List files in given directory, and add them in tree
+	 */
+	private TreeItem<String> listFile(File file, TreeItem<String> node) {
+
+		if (file.isFile()) {
+			return new TreeItem<String>(convertToNodeName(file.getName()));
+		}
+
+		for (File f : file.listFiles()) {
+
+			TreeItem<String> subNode;
+			if (f.isDirectory()) {
+				
+				subNode = new TreeItem<String>(convertToNodeName(f.getName()));
+				listFile(f, subNode);
+				
+			} else {
+				
+				if (showDirectoryOnly) {
+					continue;
+				}
+				
+				subNode = new TreeItem<String>(convertToNodeName(f.getName()));
+			}
+			
+			node.getChildren().add(subNode);
+		}
+		return node;
+	}
+    
 }
