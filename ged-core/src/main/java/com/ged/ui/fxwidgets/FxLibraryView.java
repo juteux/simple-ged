6126abea93bba914635267a84b0a2d4bf9303843
xaviermichel <xavier.michel.mx440@gmail.com>
2012-10-06 18:10:47 +0200

    Add icons in tree view

--- ged-core/src/main/java/com/ged/ui/fxwidgets/FxLibraryView.java
+++ ged-core/src/main/java/com/ged/ui/fxwidgets/FxLibraryView.java
@@ -2,13 +2,20 @@ package com.ged.ui.fxwidgets;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.Properties;
 
 import javax.swing.DropMode;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import org.apache.log4j.Logger;
+
 import com.ged.Profile;
+import com.ged.ui.MainWindow;
 import com.ged.ui.controllers.LibraryViewController;
+import com.ged.ui.preview_widgets.FilePreviewerFactory.FileType;
+import com.tools.FileHelper;
+import com.tools.PropertiesHelper;
 import com.tools.RelativeFileSystemModel;
 
 import javafx.application.Application;
@@ -28,6 +35,12 @@ import javafx.stage.Stage;
 
 public class FxLibraryView extends TreeView<String> {
 
+	/**
+	 * Tree item icon size (px)
+	 */
+	private static final int TREE_ITEM_SIZE = 30;
+	
+	private static final Logger logger = Logger.getLogger(FxLibraryView.class);
 	
 	/**
 	 * TODO : fix this value usage
@@ -35,12 +48,9 @@ public class FxLibraryView extends TreeView<String> {
 	private boolean showDirectoryOnly = false;
 	
 	
-	/*
- 	private final Node rootIcon = new ImageView(
-        new Image(getClass().getResourceAsStream("folder_16.png"))
-    );
-    */
- 	
+	Properties properties = PropertiesHelper.getInstance().getProperties();
+	
+
     public FxLibraryView() {
     	buildTree();
     }
@@ -59,8 +69,8 @@ public class FxLibraryView extends TreeView<String> {
 	 * Build the library tree, according to the registered library root
 	 */
 	private void buildTree() {
-
-		TreeItem<String> rootItem = new TreeItem<String>(convertToNodeName(Profile.getInstance().getLibraryRoot()));
+		
+		TreeItem<String> rootItem = new TreeItem<String>(convertToNodeName(Profile.getInstance().getLibraryRoot()), getIconForNode(""));
 		
 		listFile(
 				new File(Profile.getInstance().getLibraryRoot()), 
@@ -83,13 +93,78 @@ public class FxLibraryView extends TreeView<String> {
 	}
 	
 	
+ 	private Node getIconForNode(String filePath) {
+ 		
+ 		//logger.info(filePath);
+ 		
+ 		// root
+ 		if (filePath == "") {
+			Image i = new Image(getClass().getResourceAsStream(properties.getProperty("ico_library_root")));
+			ImageView iv = new ImageView(i);
+			iv.setSmooth(true);
+			iv.setFitWidth(TREE_ITEM_SIZE);
+			iv.setFitHeight(TREE_ITEM_SIZE);
+			return iv;
+ 		}
+ 		
+ 		// folder
+ 		if (new File(filePath).isDirectory()) {
+			Image i = new Image(getClass().getResourceAsStream(properties.getProperty("ico_library_folder")));
+			ImageView iv = new ImageView(i);
+			iv.setSmooth(true);
+			iv.setFitWidth(TREE_ITEM_SIZE);
+			iv.setFitHeight(TREE_ITEM_SIZE);
+			return iv;
+ 		}
+ 		
+ 		if (new File(filePath).isFile()) {
+ 			
+ 			String extension = FileHelper.getExtension(filePath);
+ 			Image i = null;
+ 			
+ 			if (extension.equals("PDF")) {
+ 				i = new Image(getClass().getResourceAsStream(properties.getProperty("ico_file_pdf")));
+ 			}
+ 			/*/else if (extension.equals("HTML")) {
+ 				return FileType.HTML_TYPE;
+ 			}
+ 			if (extension.equals("TXT")) {
+ 				return FileType.TEXT_TYPE;
+ 			}*/
+ 			else { // more complex types
+ 				
+ 				String[] imagesExtensions = new String[] { "PNG", "JPG", "JPEG", "BMP", "GIF" };
+ 				for (String s : imagesExtensions) {
+ 					if (extension.equals(s)) {
+ 						i = new Image(getClass().getResourceAsStream(properties.getProperty("ico_file_img")));
+ 						break;
+ 					}
+ 				}
+ 			}
+ 			
+ 			if (i == null) { // we haven't found a matched type
+ 				i = new Image(getClass().getResourceAsStream(properties.getProperty("ico_file_unknown")));
+ 			}
+ 			
+ 			// apply image
+			ImageView iv = new ImageView(i);
+			iv.setSmooth(true);
+			iv.setFitWidth(TREE_ITEM_SIZE);
+			iv.setFitHeight(TREE_ITEM_SIZE);
+			return iv;
+ 		}
+ 		
+ 		return null;
+ 	}
+	
+	
 	/**
 	 * List files in given directory, and add them in tree
 	 */
 	private TreeItem<String> listFile(File file, TreeItem<String> node) {
 
 		if (file.isFile()) {
-			return new TreeItem<String>(convertToNodeName(file.getName()));
+			return new TreeItem<String>(convertToNodeName(file.getName()), getIconForNode(file.getPath()));
 		}
 
 		for (File f : file.listFiles()) {
@@ -97,7 +172,7 @@ public class FxLibraryView extends TreeView<String> {
 			TreeItem<String> subNode;
 			if (f.isDirectory()) {
 				
-				subNode = new TreeItem<String>(convertToNodeName(f.getName()));
+				subNode = new TreeItem<String>(convertToNodeName(f.getName()), getIconForNode(f.getPath()));
 				listFile(f, subNode);
 				
 			} else {
@@ -106,7 +181,7 @@ public class FxLibraryView extends TreeView<String> {
 					continue;
 				}
 				
-				subNode = new TreeItem<String>(convertToNodeName(f.getName()));
+				subNode = new TreeItem<String>(convertToNodeName(f.getName()), getIconForNode(f.getPath()));
 			}
 			
 			node.getChildren().add(subNode);
