0dd1bab08f8bb613b49b4650166d904eb603b3e3
xaviermichel <xavier.michel.mx440@gmail.com>
2012-10-18 22:54:46 +0200

    fix inclusion warning

--- ged-core/src/main/java/com/tools/FileHelper.java
+++ ged-core/src/main/java/com/tools/FileHelper.java
@@ -2,9 +2,7 @@ package com.tools;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
