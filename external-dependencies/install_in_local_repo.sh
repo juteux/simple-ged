5dfb4c02876b2cd253da7fb67815c7218e7eb2c7
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2012-11-13 21:42:55 +0100

    Updating dependencies management

---
+++ external-dependencies/install_in_local_repo.sh
@@ -0,0 +1,13 @@
+mvn install:install-file -Dfile=PDFRenderer-0.9.1.jar -Dversion=0.9.1 -DgroupId=com.sun -DartifactId=PDFRenderer -DgeneratePom=true -Dpackaging=jar
+
+
+mvn install:install-file -Dfile=JTwain.jar -Dversion=1.0.0 -DgroupId=com.asprise -DartifactId=JTwain -DgeneratePom=true -Dpackaging=jar
+
+
+mvn install:install-file -Dfile=jpedal_lgpl.jar -Dversion=1.0.0 -DgroupId=org.jpedal -DartifactId=jpedal -DgeneratePom=true -Dpackaging=jar
+
+
+mvn install:install-file -Dfile=jfxrt.jar -Dversion=2.2 -DgroupId=com.oracle -DartifactId=javafx -DgeneratePom=true -Dpackaging=jar
+
+
+
