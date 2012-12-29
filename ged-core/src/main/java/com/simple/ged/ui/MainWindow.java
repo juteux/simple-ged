a93c03cfb6bd996e907ce55fb98e3f3530848f22
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2012-12-29 22:02:29 +0100

    Issue #2

--- ged-core/src/main/java/com/simple/ged/ui/MainWindow.java
+++ ged-core/src/main/java/com/simple/ged/ui/MainWindow.java
@@ -44,7 +44,7 @@ public class MainWindow extends Application {
 	/**
 	 * Default application width
 	 */
-	public static final int APP_WIDTH = 800;
+	public static final int APP_WIDTH = 930;
 	
 	/**
 	 * Default application height
