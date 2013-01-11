20a4a300c1300fe744ee8b3a05a006ed8a6314e9
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-01-11 23:16:02 +0100

    First UI throw

--- ged-core/src/main/java/com/simple/ged/ui/screen/DirectoryEditionScreen.java
+++ ged-core/src/main/java/com/simple/ged/ui/screen/DirectoryEditionScreen.java
@@ -2,6 +2,13 @@ package com.simple.ged.ui.screen;
 
 import java.util.Map;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,9 +41,24 @@ public class DirectoryEditionScreen extends SoftwareScreen {
 	private Label folderNameLabel;
 	
 	/**
+	 * Label action : change icon
+	 */
+	private Label labelChangeIcon;
+	
+	/**
+	 * Label "download from Internet"
+	 */
+	private Label downloadIconFromWebLabel;
+	
+	/**
 	 * Online icon selector
 	 */
+	private TextField editIconOnlineLocation;
 	
+	/**
+	 * Online icon download button
+	 */
+	private Button downloadOnlineIcon;
 	
 	
 	/**
@@ -49,6 +71,19 @@ public class DirectoryEditionScreen extends SoftwareScreen {
 		
 		// make sure the target exists
 		FileHelper.createDirectoryIfNecessary(DIRECTORY_ICON_DIRECTORY);
+		
+		
+		instantiateWidgets();
+	
+		HBox httpDownloadBox = new HBox();
+		httpDownloadBox.getChildren().addAll(downloadIconFromWebLabel, editIconOnlineLocation, downloadOnlineIcon);
+		
+		HBox.setHgrow(httpDownloadBox, Priority.ALWAYS);
+		
+		VBox mainLayout = new VBox();
+		mainLayout.getChildren().addAll(folderNameLabel, labelChangeIcon, httpDownloadBox);
+		
+		this.getChildren().addAll(mainLayout);
 	}
 
 
@@ -57,8 +92,28 @@ public class DirectoryEditionScreen extends SoftwareScreen {
 		directoryRelativeLocation = (String) extras.get("relative-directory-root");
 		logger.trace("Pull extra : {}", directoryRelativeLocation);
 		
+		folderNameLabel.setText(properties.getProperty("folder_edition_of").replace("{0}", directoryRelativeLocation));
 	}
 
+
+	/**
+	 * Instantiate attributes (widgets)
+	 */
+	private void instantiateWidgets() {
+		
+		folderNameLabel = new Label(properties.getProperty("folder_edition_of").replace("{0}", "?"));
+		folderNameLabel.getStyleClass().add("title-style");
+		
+		labelChangeIcon = new Label(properties.getProperty("change_icon"));
+		labelChangeIcon.getStyleClass().add("subtitle-style");
+		
+		downloadIconFromWebLabel = new Label(properties.getProperty("icon_from_internet_download"));
+		
+		editIconOnlineLocation = new TextField();
+		editIconOnlineLocation.setPromptText("http://");
+		
+		downloadOnlineIcon = new Button(properties.getProperty("download_folder_icon"));
+	}
 	
 	
 }
