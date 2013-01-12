8778f91078e5726e0cc0eb8dee66795a1530f6c3
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-01-12 17:06:24 +0100

    Add model and DAO

---
+++ ged-core/src/main/java/com/simple/ged/dao/DirectoryIconDAO.java
@@ -0,0 +1,132 @@
+package com.simple.ged.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.simple.ged.models.GedDirectory;
+import com.simple.ged.models.GedDocument;
+import com.simple.ged.models.GedDocumentFile;
+import com.simple.ged.models.GedDocumentPhysicalLocation;
+
+import fr.xmichel.toolbox.hibernate.sqlite.HibernateUtil;
+
+/**
+ * DAO for directory icon customization
+ * 
+ * @author xavier
+ * 
+ */
+public final class DirectoryIconDAO {
+
+	
+	private static final Logger logger = LoggerFactory.getLogger(DirectoryIconDAO.class);
+	
+	/**
+	 * Should not be instantiated
+	 */
+	private DirectoryIconDAO() {
+	}
+	
+	/**
+	 * 
+	 * @param directoryPath
+	 *            The directory path, relative to ged root
+	 */
+	public static synchronized GedDirectory findDirectorybyFilePath(String directoryPath) {
+		logger.debug("Get directory for file : " + directoryPath);
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Criteria criteria = session.createCriteria(GedDirectory.class).add(Restrictions.eq("relativeDirectoryPath", directoryPath));  
+		
+		@SuppressWarnings("unchecked")
+		List<GedDirectory> results = criteria.list();  
+
+		GedDirectory d = null;
+
+		if ( ! results.isEmpty() ) {
+			d = results.get(0);
+		}
+		
+		session.close();
+		
+		return d;
+	}
+
+	/**
+	 * Save or update document
+	 * @param document
+	 * 				The document to save or to update
+	 */
+	public static synchronized void saveOrUpdate(GedDirectory directory)
+	{
+		logger.debug("save directory");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.saveOrUpdate(directory);
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+
+	/**
+	 * Rename a directory (same treatment)
+	 * 
+	 * @param oldName
+	 *            The old name contains the relative path to file
+	 * 
+	 * @param newName
+	 *            The new name contains the relative path to file, with the new
+	 *            name =)
+	 */
+	public static synchronized void updateDirectoryPath(String oldName, String newName) {
+		
+		logger.debug("Rename : " + oldName + " to " + newName);
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Criteria criteria = session.createCriteria(GedDirectory.class).add(Restrictions.like("relativeDirectoryPath", oldName + "%"));  
+		
+		@SuppressWarnings("unchecked")
+		List<GedDirectory> results = criteria.list();  
+		
+		for (GedDirectory directory : results) {
+			directory.setRelativeDirectoryPath(directory.getRelativeDirectoryPath().replaceFirst(oldName, newName));
+			session.update(directory);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	
+	/**
+	 * Delete a document file
+	 * 
+	 * If some document as not at least on attached file after delete, it's removed
+	 */
+	public static synchronized void deleteDirectory(String directoryPath) {
+		logger.debug("Remove directory : " + directoryPath);
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Criteria criteria = session.createCriteria(GedDirectory.class).add(Restrictions.like("relativeDirectoryPath", directoryPath + "%"));  
+		
+		@SuppressWarnings("unchecked")
+		List<GedDirectory> results = criteria.list();  
+		
+		for (GedDirectory directory : results) {
+			session.delete(directory);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+}
---
+++ ged-core/src/main/java/com/simple/ged/models/GedDirectory.java
@@ -0,0 +1,100 @@
+package com.simple.ged.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Index;
+
+
+
+/**
+ * Some directory in the library
+ * 
+ * @author xavier
+ *
+ */
+@Entity
+@Table(name="ged_direcory")
+public class GedDirectory {
+
+	/**
+	 * The ID
+	 */
+    @Id
+    @GeneratedValue
+    @Column(name="rowid")
+	private Integer id;
+	
+	/**
+	 * The directory path, relative to the library root
+	 */
+    @Column(name="path")
+    @Index(name="idx_directory_file_path")
+	private String relativeDirectoryPath;
+
+	/**
+	 * The icon file path
+	 */
+    @Column(name="icon_path")
+	private String iconPath;
+	
+	
+	private GedDirectory() {
+		id = null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRelativeDirectoryPath() {
+		return relativeDirectoryPath;
+	}
+
+	public void setRelativeDirectoryPath(String relativeDirectoryPath) {
+		this.relativeDirectoryPath = relativeDirectoryPath;
+	}
+
+	public String getIconPath() {
+		return iconPath;
+	}
+
+	public void setIconPath(String iconPath) {
+		this.iconPath = iconPath;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		GedDirectory other = (GedDirectory) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
+}
