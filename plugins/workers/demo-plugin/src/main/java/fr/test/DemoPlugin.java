fd8cae66ec7af393adf846f565633eac37290cac
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-01-28 18:08:39 +0100

    Moving plugins in sub directory

---
+++ plugins/workers/demo-plugin/src/main/java/fr/test/DemoPlugin.java
@@ -0,0 +1,90 @@
+package fr.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.simple.ged.connector.plugins.getter.SimpleGedGetterPlugin;
+import com.simple.ged.connector.plugins.getter.SimpleGedGetterPluginProperty;
+import com.simple.ged.connector.plugins.feedback.SimpleGedPluginException;
+
+/**
+ * The demo plugin just copy the file designed by file_to_copy in the selected ged directory
+ * 
+ * @author xavier
+ *
+ */
+public class DemoPlugin extends SimpleGedGetterPlugin {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoPlugin.class);
+	
+	@Override
+	public void doGet() throws SimpleGedPluginException {
+		logger.info("Demo plugin launched...");
+		
+		// open the source
+		try (FileInputStream sourceFile = new FileInputStream(new File(getPropertyValue("file_to_copy")))) {
+			
+			// open the destination
+			try (FileOutputStream destinationFile = new FileOutputStream(getDestinationFile())) {
+
+				// perform the copy (buffered)
+				byte buffer[] = new byte[512 * 1024];
+				int readCount;
+				
+				while ((readCount = sourceFile.read(buffer)) != -1){
+					destinationFile.write(buffer, 0, readCount);
+				}
+				
+			}
+			catch (IOException e) {
+				logger.error("Cannot open destination file", e);
+				throw new SimpleGedPluginException("Impossible d'écrire dans le fichier de destination");
+			}
+			
+		} catch (IOException e){
+			logger.error("Cannot open source file ", e);
+			throw new SimpleGedPluginException("Impossible de lire le fichier source");
+		}
+		
+		logger.info("End of demo plugin");
+	}
+	
+	
+	// for testing
+	public static void main(String[] arg) {
+		
+		// Instantiate our plugin
+		SimpleGedGetterPlugin p = new DemoPlugin();
+		
+		// create properties list 
+		List<SimpleGedGetterPluginProperty> properties = new ArrayList<>();
+		
+		// create the required properties
+		SimpleGedGetterPluginProperty fileToCopy  = new SimpleGedGetterPluginProperty();
+		fileToCopy.setPropertyKey("file_to_copy");
+		fileToCopy.setPropertyValue("D:\\foo.txt");
+		
+		// add the property in list
+		properties.add(fileToCopy);
+		
+		// set properties list to our plugin
+		p.setProperties(properties);
+		
+		// define destination file for the try
+		p.setDestinationFile("D:\\toto.txt");
+		
+		// finally, try our plugin
+		try {
+			p.doGet();
+		} catch (SimpleGedPluginException e) {
+			logger.error("Epic fail :", e);
+		}
+	}
+}
